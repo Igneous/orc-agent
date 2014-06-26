7f3877f29df926c4a663843d14dea13fbbf970a9
Bucky Wolfe <bwolfe@franklinamerican.com>
2014-06-26 16:58:40 -0500

    add -config parameter

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
+import "flag"
+
 func main() {
-  config := loadConfig("config.json")
+  configfile := flag.String("config", "/etc/orc-agent/config.json",
+                            "path to orc-agent's json config file")
+  flag.Parse()
+
+  config := loadConfig(*configfile)
   conn   := amqpConnect(config.Amqpurl)
 
   for msg := range merge(amqpFollowQueues(conn, config.Queues)) {
     handleMsg(config.Handlerdir, msg)
   }
-
 }
